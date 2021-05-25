ad81f718c63b290afbd0530a90b8985090a85f1e
alex <irish.alexander.m@gmail.com>
2021-05-25 14:04:02 -0700

    updated i3 config

--- i3-polybar/polybar/launch.sh
+++ i3-polybar/polybar/launch.sh
@@ -5,7 +5,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # launch polybar
-polybar bottom >>/tmp/polybarBottom.log 2>&1
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -q --reload bottom &
+  done
+else
+  polybar -q bottom >>/tmp/polybarBottom.log 2>&1
+fi
 
 echo "Bars launched..."
 
